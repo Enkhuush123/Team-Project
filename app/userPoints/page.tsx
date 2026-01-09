88631eb0a87dbfb8e4e05f2f01c65e299805bd9d
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2026-01-09 10:45:33 +0800

    1

--- app/userPoints/page.tsx
+++ app/userPoints/page.tsx
@@ -1,12 +1,7 @@
-
 const userPoints = () => {
-
-
-
   return (
     <div className="w-200 h-100 border border-gray-500 rounded-xl mx-auto mt-20 p-5">
       <p className=" text-white">History</p>
-
     </div>
   );
 };
