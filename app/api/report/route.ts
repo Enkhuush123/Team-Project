4f3ad7ddb3579d655b7ddfad2ebda63d6eeade71
25LP7910 25LP7910 <25LP7910@admins-iMac-3.local>
2026-01-28 10:16:32 +0800

    userSchema

---
+++ app/api/report/route.ts
@@ -0,0 +1,53 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const { title, description, email } = await req.json();
+
+  if (!title) {
+    return NextResponse.json({ message: "Reason required" }, { status: 400 });
+  }
+
+  const report = await prisma.report.create({
+    data: {
+      title,
+      email,
+      description,
+      reporterId: user.id,
+    },
+  });
+
+  return NextResponse.json(report, { status: 201 });
+}
+export async function GET() {
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const [reports, reportsCount] = await Promise.all([
+    prisma.report.findMany({
+      orderBy: { createdAt: "desc" },
+      include: {
+        reporter: {
+          select: { email: true },
+        },
+      },
+    }),
+    prisma.report.count(),
+  ]);
+
+  return NextResponse.json(
+    {
+      reports,
+      reportsCount,
+    },
+    { status: 200 },
+  );
+}
