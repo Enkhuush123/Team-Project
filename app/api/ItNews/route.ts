cb0ae059a39bf8cef42456e2af7965f52ec0d6b1
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2026-01-28 12:16:28 +0800

    news

--- app/api/ItNews/route.ts
+++ app/api/ItNews/route.ts
@@ -1,32 +1,37 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const apiKey = process.env.GNEWS_API_KEY;
+  try {
 
-  if (!apiKey) {
-    return NextResponse.json(
-      { error: "GNEWS_API_KEY missing" },
-      { status: 500 },
-    );
+    const apiKey = process.env.GNEWS_API_KEY;
+
+    if (!apiKey) {
+      return NextResponse.json(
+        { error: "GNEWS_API_KEY missing" },
+        { status: 500 },
+      );
+    }
+
+    const url = `https://gnews.io/api/v4/top-headlines?category=technology&lang=en&country=us&max=6&apikey=${apiKey}`;
+
+    const res = await fetch(url, { cache: "no-store" });
+    const data = await res.json();
+
+    if (!data.articles) return NextResponse.json([]);
+
+    const formatted = data.articles.map((a: any, i: number) => ({
+      id: String(i),
+      title: a.title,
+      summary: a.description,
+      content: a.content,
+      image: a.image || "/no-image.png",
+      url: a.url,
+      source: a.source?.name,
+      publishedAt: a.publishedAt,
+    }));
+
+    return NextResponse.json(formatted);
+  } catch (err) {
+    return NextResponse.json(err, { status: 500 })
   }
-
-  const url = `https://gnews.io/api/v4/top-headlines?category=technology&lang=en&country=us&max=6&apikey=${apiKey}`;
-
-  const res = await fetch(url, { cache: "no-store" });
-  const data = await res.json();
-
-  if (!data.articles) return NextResponse.json([]);
-
-  const formatted = data.articles.map((a: any, i: number) => ({
-    id: String(i),
-    title: a.title,
-    summary: a.description,
-    content: a.content,
-    image: a.image || "/no-image.png",
-    url: a.url,
-    source: a.source?.name,
-    publishedAt: a.publishedAt,
-  }));
-
-  return NextResponse.json(formatted);
 }
