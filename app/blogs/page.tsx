3bde23281ff9fa690f0f9d98aea07ebe1f33dcfe
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2026-02-03 11:51:29 +0800

    blogs are now shareable

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -15,7 +15,7 @@ import {
   ArrowBigDown,
 } from "lucide-react";
 import { NextResponse } from "next/server";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 type Blog = {
   id: string;
@@ -43,6 +43,7 @@ type CommentItem = {
     imageUrl: string | null;
   };
 };
+
 function CommentSection({ blogId }: { blogId: string }) {
   const [loading, setLoading] = useState(false);
   const [sending, setSending] = useState(false);
@@ -162,6 +163,8 @@ function CommentSection({ blogId }: { blogId: string }) {
   );
 }
 
+export { CommentSection };
+
 export default function Blogs() {
   const [blogs, setBlogs] = useState<Blog[]>([]);
 
@@ -171,6 +174,7 @@ export default function Blogs() {
   const [showSaved, setShowSaved] = useState<string[]>([]);
 
   const router = useRouter();
+  const pathName = usePathname();
 
   useEffect(() => {
     const getBlogs = async () => {
@@ -235,6 +239,18 @@ export default function Blogs() {
     setShowSaved((prev) => [...prev, blogId]);
   };
 
+  const handleShare = async (blogId: string) => {
+    try {
+      await navigator.clipboard.writeText(
+        `localhost:3000${pathName}/${blogId}`,
+      ); // insert the domain link before pathname here
+    } catch (err) {
+      console.log(err, "failed to copy");
+    }
+
+    window.alert("Link Copied");
+  };
+
   return (
     <div className="min-h-screen bg-black text-white">
       <div className="pointer-events-none fixed inset-0">
@@ -260,7 +276,6 @@ export default function Blogs() {
               key={item.id}
               className="w-full overflow-hidden rounded-2xl bg-white/5 border border-white/10 backdrop-blur-xl
                          shadow-[0_20px_60px_rgba(99,102,241,0.12)] hover:border-white/20 transition"
-              onClick={() => router.push(`/blogs/${item.id}`)}
             >
               <div
                 className="flex items-center gap-3
@@ -280,7 +295,10 @@ export default function Blogs() {
                 </div>
                 <div>{item.user.email}</div>
               </div>
-              <div className="px-5 sm:px-6 py-4 border-b border-white/10 flex items-center justify-between">
+              <div
+                className="px-5 sm:px-6 py-4 border-b border-white/10 flex items-center justify-between"
+                onClick={() => router.push(`/blogs/${item.id}`)}
+              >
                 <div className="min-w-0">
                   <h2 className="text-white font-extrabold text-lg sm:text-xl leading-snug truncate">
                     {item.title}
@@ -369,6 +387,9 @@ export default function Blogs() {
                     <button
                       className="flex items-center gap-2 text-white/60 hover:text-white transition"
                       type="button"
+                      onClick={() => {
+                        handleShare(item.id);
+                      }}
                     >
                       <Send className="h-4 w-4" />
                       <span className="text-sm">Share</span>
