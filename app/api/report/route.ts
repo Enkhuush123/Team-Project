d8f636192b88df2a47abedde8a631c9745c516ba
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2026-02-05 13:20:21 +0800

    fix

--- app/api/report/route.ts
+++ app/api/report/route.ts
@@ -3,28 +3,38 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 export async function POST(req: Request) {
-  const user = await currentUser();
-  if (!user) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const { title, description, email, blogId } = await req.json();
+
+    if (!title) {
+      return NextResponse.json({ message: "Reason required" }, { status: 400 });
+    }
+
+    const report = await prisma.report.create({
+      data: {
+        title,
+        description,
+        reportedBlog: {
+          connect: { id: blogId },
+        },
+        reporter: {
+          connect: { id: user.id },
+        },
+      },
+    });
+
+
+    return NextResponse.json(report, { status: 201 });
+
+  } catch (err) {
+    return NextResponse.json(err)
   }
 
-  const { title, description, email } = await req.json();
-
-  if (!title) {
-    return NextResponse.json({ message: "Reason required" }, { status: 400 });
-  }
-
-  const report = await prisma.report.create({
-    data: {
-      title,
-      email,
-      description,
-      reporterId: user.id,
-      userId: user.id,
-    },
-  });
-
-  return NextResponse.json(report, { status: 201 });
 }
 export async function GET() {
   const user = await currentUser();
